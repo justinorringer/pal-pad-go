6baaa6e4aade4f001e684083ffe175ae5594fd3e
Justin Orringer <justinorringer@gmail.com>
2023-03-02 16:02:49 -0500

    Added mongodb support

--- index.js
+++ index.js
@@ -1,29 +1,57 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
+import express from 'express';
+import http from 'http';
+import { Server } from "socket.io";
+import { fileURLToPath } from 'url';
 
+import Client from './mongo/mongo.js';
+
+const __dirname = fileURLToPath(import.meta.url).replace('index.js', '');
+
+const app = express();
+const server = http.createServer(app);
 const io = new Server(server);
 
-// Using in memory storage for now
-let lines = [];
+var client = new Client();
+
+await client.Connect();
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/static/index.html');
 });
 
 io.on('connection', (socket) => {
-    io.emit('sync', lines)
+    // join a room, and create a new sketch
+    // and send the sketch id to the client
+    socket.on('join', (sketch_id) => {
+        (async () => {
+            let sketch = await client.GetSketch(sketch_id);
 
-    socket.on('line', (line) => {
-        lines.push(line);
-        io.emit('sync', lines);
+            if (sketch == null) {
+                console.log("Creating new sketch");
+                sketch_id = await client.NewSketch(sketch_id);
+                console.log("New sketch id: " + sketch_id)
+            }
+
+            socket.join(sketch_id);
+
+            console.log("Joined sketch: " + sketch_id);
+
+            // emit the sketch to only the client that just joined
+            socket.emit('sketch', sketch);
+        })();
     });
 
-    socket.on('clear', () => {
-        lines = [];
-        io.emit('sync', lines)
+    socket.on('line', (sketch_id, line) => {
+        console.log("Line: " + line
+            + " from sketch: " + sketch_id)
+        // add the line to the database
+        client.NewLine(sketch_id, line);
+        io.emit('sync', line);
+    });
+
+    socket.on('clear', (sketch_id) => {
+        client.ClearLines(sketch_id);
+        io.emit('sketch', { "id": sketch_id, lines: []});
     });
 });
 
---
+++ mongo/mongo.js
@@ -0,0 +1,110 @@
+import { MongoClient } from 'mongodb';
+
+var url = "mongodb://localhost:27017/";
+var db = "pal-pad";
+var sketches_collection = "sketches";
+var lines_collection = "lines";
+
+class Client {
+    constructor() {
+        this.client = new MongoClient(url);
+    }
+
+    async Connect() {
+        try {
+            await this.client.connect();
+            console.log("Connected successfully to server");
+
+            this.db = this.client.db(db);
+        } catch (err) {
+            console.log(err.stack);
+        }
+    }
+
+    async NewSketch(id) {
+        try {
+            const collection = this.db.collection(sketches_collection);
+
+            const result = await collection.insertOne({ _id: id, lines: [] });
+            console.log(result);
+
+            return await this.GetSketch(id);
+        } catch (err) {
+            console.log(err.stack);
+        }
+    }
+
+    async GetSketch(id) {
+        try {
+            const s_collection = this.db.collection(sketches_collection);
+
+            const sketch_table = await s_collection.findOne({ _id: id })
+            
+            if (sketch_table == null) {
+                return null;
+            }
+
+            // now that we have the sketch_table,
+            // we need to get the lines from the lines table
+            // and return everything as a single object
+            // sketch: { id, lines: [line1, line2, line3] }
+
+            const l_collection = this.db.collection(lines_collection);
+
+            const lines_array = await l_collection.find({ _id: { $in: sketch_table.lines } }).map((element, _) => {
+                return element.line;
+            }).toArray();
+
+            let result = {
+                id: sketch_table._id,
+                lines: lines_array
+            }
+            return result;
+        } catch (err) {
+            console.log(err.stack);
+        }
+    }
+
+    async indexLine(id, line_id) {
+        try {
+            const sketches = this.db.collection(sketches_collection);
+
+            const result = await sketches.updateOne(
+                { _id: id }, 
+                { $push: { lines: line_id } });
+
+            console.log(result);
+        } catch (err) {
+            console.log(err.stack);
+        }
+    }
+
+    async NewLine(sketch_id, line) {
+        try {
+            const lines = this.db.collection(lines_collection);
+
+            const result = await lines.insertOne({ line: line });
+
+            this.indexLine(sketch_id, result.insertedId);
+        } catch (err) {
+            console.log(err.stack);
+        }
+    }
+
+    async ClearLines(sketch_id) {
+        try {
+            const sketches = this.db.collection(sketches_collection);
+
+            const result = await sketches.updateOne(
+                { _id: sketch_id },
+                { $set: { lines: [] } }
+            );
+
+            console.log(result);
+        } catch (err) {
+            console.log(err.stack);
+        }
+    }
+}
+
+export default Client;
